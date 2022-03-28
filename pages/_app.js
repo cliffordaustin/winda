7185ef1546cfe620c2d62f46825315de04e7e7c4
Clifford <kingsleyclifford2016@gmail.com>
2022-03-28 22:20:36 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,7 @@ import { Mixpanel } from "../lib/mixpanelconfig";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    console.log("Token mapbox ", process.env.NEXT_PUBLIC_MAPBOX_API_KEY);
-    //Mixpanel.register_once({ "First Login Date": new Date().toISOString() });
+    Mixpanel.register_once({ "First Login Date": new Date().toISOString() });
   }, []);
   return <Component {...pageProps} />;
 }
