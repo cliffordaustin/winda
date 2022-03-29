e5f3e1dad47038116fbb94643535994824e42d85
Clifford <kingsleyclifford2016@gmail.com>
2022-03-29 00:28:52 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,6 @@ import mixpanel from "mixpanel-browser";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // NB: CHANGE DEBUG TO FALSE IN PRODUCTION
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
-      debug: true,
-      ignore_dnt: true,
-    });
     Mixpanel.register_once({ "First Login Date": new Date().toISOString() });
   }, []);
   return <Component {...pageProps} />;
