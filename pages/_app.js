b81e93f22e19fe07ac488f3c0fa91d61a84fd32e
Clifford <kingsleyclifford2016@gmail.com>
2022-03-29 09:13:10 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import { useEffect } from "react";
 import "../styles/globals.css";
 import { Mixpanel } from "../lib/mixpanelconfig";
+import mixpanel from "mixpanel-browser";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    Mixpanel.register_once({ "First Login Date": new Date().toISOString() });
+    // NB: CHANGE DEBUG TO FALSE IN PRODUCTION
+    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
+      debug: true,
+      ignore_dnt: true,
+    });
+    mixpanel.register_once("Register Once", {
+      "First Login Date": new Date().toISOString(),
+    });
   }, []);
   return <Component {...pageProps} />;
 }
