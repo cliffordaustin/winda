e4eee365d2a1620293aec01c16c4f6a1b35fbe92
cliffordaustin <cliffordaustin670@gmail.com>
2022-07-07 03:18:46 +0000

    initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Head from "next/head";
 import Cookies from "js-cookie";
 import axios from "axios";
