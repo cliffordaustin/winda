ffc40e1ddf7902abb1496855b284b42b82ce029d
cliffordaustin <cliffordaustin670@gmail.com>
2022-08-07 15:38:15 +0000

    initial commit

--- components/Home/Dialogue.jsx
+++ components/Home/Dialogue.jsx
@@ -29,10 +29,10 @@ const Dialogue = ({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25"></div>
+            <div className="fixed safari-bottom inset-0 bg-black bg-opacity-25"></div>
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed safari-bottom inset-0 overflow-y-auto">
             <div
               className={
                 "flex min-h-full items-center justify-center p-4 text-center " +
