85fd098bdd60483a0be32661411918ab15905882
cliffordaustin <cliffordaustin670@gmail.com>
2022-09-01 01:43:06 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -7,4 +7,15 @@ module.exports = {
       "lh3.googleusercontent.com",
     ],
   },
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? {
+            source: "/((?!maintenance).*)",
+            destination: "/maintenance.html",
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
+  },
 };
