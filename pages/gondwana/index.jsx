57ad8dedeb0c44224fd6b92b2f70db96dfe7bc9a
cliffordaustin <cliffordaustin670@gmail.com>
2022-09-21 17:21:59 +0000

    initial commit

--- pages/gondwana/index.jsx
+++ pages/gondwana/index.jsx
@@ -412,6 +412,31 @@ function RequestTrip({ userProfile, stays }) {
               accordingly. Also, we recommend you wear suitable footwear as they
               ground might be uneven.{" "}
             </p>
+
+            <div className="fixed top-3 right-4 flex flex-col">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setShowFAQ(false);
+                }}
+                className="flex cursor-pointer items-center justify-center w-7 h-7 rounded-full bg-white shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
           </Dialogue>
           <div
             onClick={() => {
@@ -524,6 +549,31 @@ function RequestTrip({ userProfile, stays }) {
               There will be no refunds for any accommodation booked and no
               transfer of names associated with bookings. All sales are final.
             </p>
+
+            <div className="fixed top-3 right-4 flex flex-col">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setShowTerms(false);
+                }}
+                className="flex cursor-pointer items-center justify-center w-7 h-7 rounded-full bg-white shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
           </Dialogue>
         </div>
       </div>
